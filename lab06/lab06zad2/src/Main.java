bdb53cfb8270f9b55c2622199212ae7f2993e39e
Konrad Pluta <konrad.k.j.pluta@gmail.com>
2023-05-21 16:28:15 +0200

    Revert "Dodanie zadan 2,3 z lab06"
    This reverts commit 04b4567262cca88ce59607ec7021fdc0cb39877b.

--- lab06/lab06zad2/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Samochod samochod = new Samochod();
-        SamochodOsobowy samochodOsobowy = new SamochodOsobowy();
-        samochod.informacje();
-        samochodOsobowy.informacje();
-
-    }
-}
